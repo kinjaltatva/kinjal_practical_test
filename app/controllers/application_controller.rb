212843b9decf223140b713d4f7d5e41d8e37adce
Kinjal Thakkar <kinjal.thakkar@tatvasoft.com>
2020-01-29 11:46:07 +0530

    implemneted devise and doorkeeper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  protect_from_forgery prepend: true, if: Proc.new { |c| c.request.format.include? 'application/json'}
+  before_action :doorkeeper_authorize!
 end
